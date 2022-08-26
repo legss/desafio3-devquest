0ae38b026a2c46355e2d97d0ccc39b3cb6851c51
legss-linux <luiz_gss@outlook.com>
2022-08-25 22:26:26 -0300

    add README

---
+++ README.md
@@ -0,0 +1,29 @@
+# Desafio JS Intermediario
+
+Este desafio, faz parte do treinamento Dev Quest, realizado pelo [Dev-em-Dobro](https://www.youtube.com/c/DevemDobro).
+
+## Sumário
+
+  - [Screenshot](#screenshot)
+  - [Live Site](#live-site)
+  - [O que foi usado?](#o-que-foi-usado?)
+  - [O que eu aprendi](#o-que-eu-aprendi)
+
+## Visão Geral
+
+### Screenshot
+
+![](src/imagens/desktop-animation.gif)
+
+
+### Live Site
+
+Caso quiser ver como ficou o mini-projeto, eu postei [aqui]()
+
+
+### O que foi usado?
+
+- HTML
+- CSS
+- JavaScript
+
--- src/js/campo-obrigatorio.js
+++ src/js/campo-obrigatorio.js
@@ -1,22 +1,24 @@
 const inputs = document.querySelectorAll('.input')
 const form = document.getElementById('form')
-console.log(form)
-
+const obrigatorios = document.querySelectorAll('.obrigatorio')
 const submit = document.getElementById('submit')
 
+
     form.addEventListener("submit", function(evento) {
-        evento.preventDefault();
         inputs.forEach(input => {
             if (input.value == "") {
+                evento.preventDefault();
+                input.classList.remove('green-border')
                 input.classList.add('red-border')
+                input.nextElementSibling.classList.add('mostrar')
+
             } else {
+                input.nextElementSibling.classList.remove('mostrar')
                 input.classList.remove('red-border')
                 input.classList.add('green-border')
-            }
+            }   
+
         });
     
     })
-
-
-  
 
